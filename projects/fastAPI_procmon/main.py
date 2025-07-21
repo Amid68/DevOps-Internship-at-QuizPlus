563d0c1605b5c7aab86b5cf45f72f5c2d83c4fb4
Ameed Othman <othman.ameed@gmail.com>
2025-07-21 14:09:06 +0300

    feat(fastAPI): create a simple FastAPI application that returns running processes The project is a FastAPI application that recieves a GET request and returns current running processes (max 50) along with their memory consumption in json format. It includes a main.py that has the application logic, Dockerfile that builds an image from this project and .gitignore as well as .dockerignore files. Finally requirements.txt contains the necessary libraries.

---
+++ projects/fastAPI_procmon/main.py
@@ -0,0 +1,37 @@
+######################################################
+#
+# @file main.py
+# @brief simple FastAPI app that returns 
+# running processes with their memory  consumption
+#
+# @author Ameed Othman
+#
+######################################################
+
+from fastapi import FastAPI
+import psutil # this library enables us to get info on running processes
+
+app = FastAPI()
+
+def get_processes_by_memory(limit=50):
+    """Get top processes by memory consumption"""
+    processes = []
+
+    for proc in psutil.process_iter(['name', 'memory_percent']):
+        try:
+            processes.append({
+                'name': proc.info['name'],
+                'memory_percent': proc.info['memory_percent']
+            })
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            # Skip processes that can't be accessed
+            pass
+
+    # Sort by memory percentage (descending) and get top N
+    top_processes = sorted(processes, key=lambda x: x['memory_percent'], reverse=True)[:limit]
+
+    return top_processes
+
+@app.get("/processes")
+def get_procs():
+    return get_processes_by_memory()
