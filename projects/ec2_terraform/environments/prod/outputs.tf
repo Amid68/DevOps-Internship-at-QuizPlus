8f7aefc6c74b08c58de3781d68ae434a0a828829
Ameed Othman <othman.ameed@gmail.com>
2025-08-04 10:07:26 +0300

    debug

--- projects/ec2_terraform/environments/prod/outputs.tf
+++ projects/ec2_terraform/environments/prod/outputs.tf
@@ -1,14 +1,14 @@
 output "ec2_instance_id" {
   description = "ID of the EC2 instance"
-  value       = module.ec2.aws_instance.this.id
+  value       = module.ec2.ec2_instance_id
 }
 
 output "ec2_public_ip" {
-  description = "Public IP address"
-  value       = module.ec2.aws_instance.this.public_ip
+  description = "Public IP address of the EC2 instance"
+  value       = module.ec2.ec2_public_ip
 }
 
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = module.security_group.aws_security_group.ec2.id
+output "ec2_private_ip" {
+  description = "Private IP address of the EC2 instance"
+  value       = module.ec2.ec2_private_ip
 }
